ef28c3ce43b036972cabe14f647c281fa70a4398
dipesh <equesater2024@gmail.com>
2025-11-05 11:45:12 +0545

    Feat: Connect and wire db to server entry, Solve querySrv ECONNREFUSED error.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
+import dns from 'dns';
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 dotenv.config();
+connectDB();
 
 const app = express();
 app.use(express.json());

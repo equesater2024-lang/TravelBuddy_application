917b30984a094ce84503db2e622538bcbc358bfd
dipesh <equesater2024@gmail.com>
2025-11-19 16:37:04 +0545

    Feat: Wire Itinerary routes into server.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.js';
+import itineraryRoutes from './routes/itineraries.js';
 
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('TravelBuddy API running'));
 app.use('/api/auth', authRoutes);
+app.use('/api/itineraries', itineraryRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
